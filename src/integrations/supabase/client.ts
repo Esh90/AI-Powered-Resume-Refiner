0ee6e0903a87cdfc409565dc881cc698a36604a2
Esh90 <145446863+Esh90@users.noreply.github.com>
2025-07-31 11:03:31 +0500

    Debugged supabase API errors

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://mmlgusykdigxskqxpqyw.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tbGd1c3lrZGlneHNrcXhwcXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MzU2OTksImV4cCI6MjA2OTIxMTY5OX0.MLTaXroP3P07g_jW8YUB1ivQy9lqaIZ6QbZX3Vn-yQg";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
